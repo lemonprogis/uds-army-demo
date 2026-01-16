89b1574def95c5a9c68cbd627bbf8c9f03f2102f
lemonprogis <edward.briggler@gmail.com>
2026-01-15 20:09:22 -0600

    add endpoint

--- app/app.py
+++ app/app.py
@@ -27,7 +27,7 @@ def root():
 def healthz():
     return jsonify(status="ok", service=SERVICE_NAME)
 
-@app.route("/testing-endpoint")
+@app.route("/testing")
 def test():
     return jsonify(test="test successful", service=SERVICE_NAME)
 
