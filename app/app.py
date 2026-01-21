ad6f422085758f8e9258e742c832c2fb6042c441
lemonprogis <edward.briggler@gmail.com>
2026-01-20 22:28:47 -0600

    test fire

--- app/app.py
+++ app/app.py
@@ -27,7 +27,7 @@ def root():
 def healthz():
     return jsonify(status="ok", service=SERVICE_NAME)
 
-@app.route("/test")
+@app.route("/testss")
 def test():
     return jsonify(test="test successful", service=SERVICE_NAME)
 
